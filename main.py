e273e95b0a467bf47eb65a3c37c2ca4e64c54128
Dedstate <96666517+Dedstate@users.noreply.github.com>
2022-06-30 21:43:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit
+from pathlib import Path
+from random import choice
+
+
+with open("src/russian2.txt", encoding='utf-8') as f:
+    ls = f.read().splitlines()
+    print(ls)
+# Сhange working directory
+cur_dir = os.path.dirname(__file__)
+os.chdir(cur_dir)
+
+
+class MyWidget(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("src/MainWindow.ui", self)
+        self.pushButton.clicked.connect(self.clicked)
+        self.word = choice(ls)
+        self.lineEdit.setText("_" * len(self.word))
+        self.text = ""
+        self.count = 0
+
+    # for i in range(len(ls)):
+    #     if len(ls[i]) != 5:
+    #         ls.pop(i)
+
+    def attempt(self):
+        if self.text not in self.word:
+            self.count += 1
+            self.lineEdit_2.setText("Вы ошиблись, осталсь попыток " + str(7 - self.count))
+        else:
+            for i in range(len(self.word)):
+                if self.text[0] == self.word[i]:
+                    t = self.lineEdit.text()
+                    self.lineEdit.setText(t[:i] + self.text[0] + t[i + 1:])
+
+        if "_" not in self.lineEdit.text():
+            self.lineEdit_2.setText("Вы победили")
+
+    def clicked(self):
+        self.text = self.input.text()
+        self.input.setText('')
+        self.attempt()
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
+
+
